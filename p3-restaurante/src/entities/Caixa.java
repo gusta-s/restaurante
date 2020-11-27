d2442465d12f9b9a667d565c2935a4534fb7bb13
laradso <54984991+laradso@users.noreply.github.com>
2020-11-27 17:55:22 -0300

    Update Caixa.java

--- p3-restaurante/src/entities/Caixa.java
+++ p3-restaurante/src/entities/Caixa.java
@@ -7,59 +7,32 @@ package entities;
 
 import java.util.Random;
 
-public class Caixa {
+public class Caixa {//implements Runnable{
 	
 	private static final int[] chances = new int[] {0,0,0,1,0}; // 20%
 	private Random r = new Random();
 		
-	private Cliente cliente;
 	private Pagamento pagamento = new Pagamento();
 	private Fila fila = new Fila();
 	
 	public Caixa() {
 		fila = new Fila();
-	}
-	
-	public Caixa(Cliente cliente) {
-		fila = new Fila();
 		pagamento = new Pagamento();
-		this.cliente = cliente;
-	}
-	
-	public Cliente getCliente() {
-		return cliente;
 	}
 
 	public Fila getFila() {
 		return fila;
 	}
 	
-	public void pagar() {
-		float preco = cliente.getPedido().getPrecoTotal();
-		
-		System.out.printf("(%s) ", cliente.getNome());
-		if (chances[r.nextInt(chances.length)] == 1) {
-			pagamento.realizarPagamento(preco, r.nextInt(20));
-		}
-		else {		
-			pagamento.realizarPagamento(preco);
-		}
-		
-		cliente.getPedido().removerPedido();
-	}
-	
 	public void pagar(Cliente cliente){
 		float preco = cliente.getPedido().getPrecoTotal();
 
-		System.out.printf("(%s) ", cliente.getNome());
 		if (chances[r.nextInt(chances.length)] == 1) {
 			this.pagamento.realizarPagamento(preco, r.nextInt(20));
 		}
 		else {		
-			
 			this.pagamento.realizarPagamento(preco);
 		}
 		cliente.getPedido().removerPedido();
 	}
-
 }
